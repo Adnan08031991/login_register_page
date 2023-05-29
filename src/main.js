d206a4e97edc788559274930bf3354c998abe0f5
Adnan08031991 <hpadnan91@gmail.com>
2023-05-29 19:02:15 +0200

    10k

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+
+const app = createApp(App);
+app.mount('#app');
+<div id="app">
+  <button @click="count++">
+    Count is: {{ count }}
+  </button>
+</div>
